7022f845133c3225bada428fce9248da51aa7ac8
Eliana Frosch <lauraelianafrosch@gmail.com>
2023-09-01 18:15:16 -0300

    modificacion ID-arreglado

--- productManager.js
+++ productManager.js
@@ -3,7 +3,6 @@ const fs = require ("fs");
 class ProductManager{
     constructor (filePath){
         this.filePath = filePath;
-        this.lastProductId = 0;
     }
 
     fileExist(){
@@ -31,8 +30,6 @@ class ProductManager{
         }
     }
 
-
-
     async addProduct (productInfo){
         
         try {
@@ -40,41 +37,36 @@ class ProductManager{
 
             // valido si el archivo existe
             if(this.fileExist()){
-
                 // lee el archivo
                 const content = await fs.promises.readFile(this.filePath,"utf8");
                 // paso de string a Json
                 const contentJson = JSON.parse(content);
                                 
                 // validar que se ingrese toda la info
-
                 if (!productInfo.title || !productInfo.description || !productInfo.price ||!productInfo.thumbnail 
                     ||!productInfo.code || !productInfo.stock){
                     console.log("Se necesita ingresar datos validos");
-
                 }else{
                     // validar que el codigo no se repita
                     const prodCode = contentJson.find((e)=>e.code===productInfo.code)
                     if(prodCode){
                         console.log("Code ya existe, vuelva a ingresar un dato correcto")
-
                     }else{
-
+                        let newId
+                        if(contentJson.length == 0){
+                            newId = 0
+                        }else{
+                            newId = contentJson[contentJson.length -1].id + 1
+                        }
                         const newProduct={
-                                title,description,price,thumbnail,code,stock,id:this.lastProductId,
+                                title,description,price,thumbnail,code,stock,id:newId,
                             }
-                
                         contentJson.push(newProduct);
-                        // this.lastProductId ++;
-
                         // sobreescribir el archivo con el nuevo producto
-                        await fs.promises.writeFile(this.filePath,JSON.stringify(contentJson,null,"\t"));
-                                            
+                        await fs.promises.writeFile(this.filePath,JSON.stringify(contentJson,null,"\t"));                    
                         console.log("Producto agregado.");
                     };
-                    
                 };
-                
             }else{
                 throw new Error ("No es posible guardar el producto");
             };
@@ -149,19 +141,18 @@ class ProductManager{
 const income = async()=>{
     try {
         const manager = new ProductManager("./fileProduct.json");
-        // const productLoaded = await manager.getProducts();
-        // console.log(productLoaded);
         
         // await manager.addProduct({title:"calza deportiva",description:"Calza deportiva mujer",price:8000, thumbnail:"No posee",code:"77901",stock:50});
         // await manager.addProduct({title:"short deportivo",description:"short deportivo mujer",price:6000, thumbnail:"No posee",code:"77902",stock:50});
         // await manager.addProduct({title:"camiseta deportiva",description:"camiseta mujer",price:4000, thumbnail:"No posee",code:"77903",stock:50});
         
-        // const productId = await manager.getProductById(2);
+        // const productId = await manager.getProductById(0);
+        // console.log(productId);
         
         // const productDeleted= await manager.deleteProduct(0);
         
-        // const productLoaded = await manager.getProducts();
-        // console.log(productLoaded);
+        const productLoaded = await manager.getProducts();
+        console.log(productLoaded);
         
     
     } catch (error) {
